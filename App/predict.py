61da2d1f31852a7eac65fd2a936f704da5694337
Sabari Kameswaran S <sabariedu19@gmail.com>
2024-04-07 22:35:31 +0530

    Test Codes Commented

--- App/predict.py
+++ App/predict.py
@@ -3,66 +3,39 @@ import os
 from tensorflow import keras
 from tensorflow.keras.models import load_model
 from werkzeug.utils import secure_filename
-#from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from flask import render_template
-#pip install pillow
 import io
-#import cloudstorage as gcs
-#from google.appengine.api import app_identity
 
 
 class plantleaf:
-    #def __init__(self, filename):
-    #    self.filename = filename
-
     def predictPlantImage(self, image_file):
 
         self.image_file = image_file
-
-        # load the model
-        # model = load_model("COFFEE_model.h5")
         model = load_model("C:\\Users\\SABARI HARI\\Downloads\\PlantDiseasePrediction\\App\\COFFEE_model.h5")
-        # model = load_model('PlantLeafCNN_2021-03-04 _Basic_CNN.h5')
-
-        #Save the file to ./uploads
-
-        basepath = os.path.dirname(__file__) # - org
-        #basepath = os.path.dirname('Last Try')
-
-        file_path = os.path.join(basepath, 'uploads', secure_filename(self.image_file.filename)) # - org
-        # secure_filename - Pass it a filename and it will return a secure version of it.
-        # This filename can then safely be stored on a regular file system and passed to os.
-
-        #file_path = os.path.dirname('Last Try/temp')
-
-        self.image_file.save(file_path)  # save the image for further use - org
-
-        test_image = image.load_img(file_path, target_size=(256, 256)) # should be same as given in the code for input
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, 'uploads', secure_filename(self.image_file.filename)) 
+        self.image_file.save(file_path)
+        test_image = image.load_img(file_path, target_size=(256, 256))
         test_image = image.img_to_array(test_image)
         test_image = test_image / 255
-        test_image = np.expand_dims(test_image, axis=0)  # expand dimension - flattening it
-
+        test_image = np.expand_dims(test_image, axis=0) 
         preds = model.predict(test_image)
-        #print(preds)
-
         preds = np.argmax(preds,axis=1)  # The numpy. argmax() function returns indices of the max element of the array in a particular axis.
         #print(preds)
 
         if preds == 0:
-            prediction = "Cerscospora. The Fertilizers are Penthiopyrad,Strobilurin,Copper Fungicides"
+            prediction = "The Disease is Cerscospora. The Fertilizers are Penthiopyrad,Strobilurin,Copper Fungicides"
             return prediction
         elif preds == 1:
             prediction = "Healthy Leaf"
             return prediction
         elif preds == 2:
-            prediction = "Leaf Rust. The Fertilizers are RANSOM,GREENCOP,DEFACTO"
+            prediction = "The Disease is Leaf Rust. The Fertilizers are RANSOM,GREENCOP,DEFACTO"
             return prediction
         elif preds == 3:
-            prediction = "Miner. The Fertilizers are Sevin, malathion, and lindane."
+            prediction = "The Disease is Miner. The Fertilizers are Sevin, malathion, and lindane."
             return prediction
-        
-        
         else:
-            prediction = "Phoma. The Fertilizers propiconazole and difenoconazole, and premixtures, pydiflumetofen + fludioxonil or pydiflumetofen + difenoconazol"
+            prediction = "The Disease is Phoma. The Fertilizers propiconazole and difenoconazole, and premixtures, pydiflumetofen + fludioxonil or pydiflumetofen + difenoconazol"
             return prediction
